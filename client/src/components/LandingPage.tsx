4fc50c857c2bab676c35e84169d92b4bba8fdae0
borschlek <borschlek@users.noreply.github.com>
2025-06-23 11:19:31 +0000

    GTM official snippet, GDPR compliant

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -82,19 +82,24 @@ export default function LandingPage() {
   // Inject GTM after consent
   useEffect(() => {
     if (cookieConsent && !gtmLoaded) {
-      // Inject GTM head
+      // Inject GTM head (official snippet)
       if (!document.getElementById("gtm-head")) {
         const script = document.createElement("script");
         script.id = "gtm-head";
-        script.async = true;
-        script.src = `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`;
-        document.head.appendChild(script);
+        script.innerHTML = `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
+        `;
+        document.head.prepend(script);
       }
       // Inject GTM body (noscript)
       if (!document.getElementById("gtm-body")) {
         const noscript = document.createElement("noscript");
         noscript.id = "gtm-body";
-        noscript.innerHTML = `<iframe src=\"https://www.googletagmanager.com/ns.html?id=${GTM_ID}\" height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe>`;
+        noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
         document.body.prepend(noscript);
       }
       setGtmLoaded(true);
